a6450e85e38a4b45c03e1d6645aacae81aca23e3
itishree2 <nayakitishree6262@gmail.com>
2024-02-19 21:23:59 +0530

    Add project files.

---
+++ SchoolLib/SchoolManagement.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolLib
+{
+    public class SchoolManagement
+    {
+        private List<Student> students = new List<Student>();
+        private List<Subject> subjects = new List<Subject>();
+        private List<Teacher> teachers = new List<Teacher>();
+
+        public List<Student> Students
+        {
+            get { return students; }
+            private set { students = value; }
+        }
+       public List<Subject> Subjects
+        {
+            get { return subjects; }
+            private set { subjects = value; }
+        }
+        public List <Teacher> Teachers
+        {
+            get { return teachers; } 
+          private set { teachers = value; } 
+        }
+        public void AddStudent(Student student)
+        { 
+            students.Add(student);
+        }
+        public void AddSubject(Subject subject)
+        {
+            subjects.Add(subject);
+        }
+        public void AddTeacher(Teacher teacher)
+        {
+            teachers.Add(teacher);
+        }
+
+    }
+}
---
+++ SchoolLib/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolLib
+{
+    public class Student
+    {
+        public virtual int StudentId { get; set; }
+        public virtual string? StudentName { get; set; }
+        public virtual string? Class { get; set; }
+    }
+}
